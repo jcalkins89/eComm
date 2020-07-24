8d89447f301fd68eb523b786380dece2340e3c13
Josh Calkins <jcalkins89@gmail.com>
2020-07-24 16:53:39 -0400

    Add Directory and Menu Item components, split out components/pages

---
+++ src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => {
+  return (
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer>
+  );
+};
+
+export default HomePage;
+
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
+`;
